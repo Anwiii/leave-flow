c2907769f3afc555d2d00f811e39fc95d1b98f8b
Anwiii <anwiamrita@gmail.com>
2026-05-10 23:53:14 +0530

    api app.vue main.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <Navbar v-if="auth.isAuthenticated" />
+    <main :class="auth.isAuthenticated ? 'pt-4' : ''">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from './store/auth'
+import Navbar from './components/Navbar.vue'
+
+const auth = useAuthStore()
+</script>
